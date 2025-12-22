742acab81a5ce4be3386a1613785a19825f001d5
GregoryAM <gregoryam.blog@gmail.com>
2025-12-21 19:11:48 -0600

    Initial Commit
    Render world using The Minecraft Overviewer

---
+++ overviewerConfig.js
@@ -0,0 +1,121 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "Stars Hollow SMP"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1766365734",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        },
+        "attribution": null
+    },
+    "tilesets": [
+        {
+            "name": "Overworld",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "overworld",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Stars Hollow SMP",
+            "last_rendertime": 1766359128,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                160,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                0,
+                160,
+                0
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "Nether",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "nether",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Stars Hollow SMP",
+            "last_rendertime": 1766309361,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                64,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": null,
+            "north_direction": 0
+        },
+        {
+            "name": "Nether Roof",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "nether_roof",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "Stars Hollow SMP",
+            "last_rendertime": 1766309361,
+            "imgextension": "webp",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                64,
+                0
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": null,
+            "north_direction": 0
+        }
+    ]
+};
